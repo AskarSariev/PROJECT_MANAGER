8d1cddc9465930789fac7fef62baf54b27fa9704
Askar Sariev <askar-64@mail.ru>
2023-03-22 21:08:04 +0300

    Create project and update file application.properties

---
+++ project-manager/src/test/java/ru/advengineering/projectmanager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.advengineering.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
