e2e8d4417e3d1b3ebf14a55192df724d18cb7c84
Askar Sariev <askar-64@mail.ru>
2023-03-22 21:52:07 +0300

    Add Entity class Task

---
+++ project-manager/src/main/java/ru/advengineering/projectmanager/models/Task.java
@@ -0,0 +1,101 @@
+package ru.advengineering.projectmanager.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "create_date")
+    private LocalDate createDate;
+
+    @Column(name = "update_date")
+    private LocalDate updateDate;
+
+    @Column(name = "executor")
+    private String executor;
+
+    public Task() {
+    }
+
+    public Task(String name, String status, LocalDate createDate, LocalDate updateDate, String executor) {
+        this.name = name;
+        this.status = status;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+        this.executor = executor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDate getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDate updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getExecutor() {
+        return executor;
+    }
+
+    public void setExecutor(String executor) {
+        this.executor = executor;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", createDate=" + createDate +
+                ", updateDate=" + updateDate +
+                ", executor='" + executor + '\'' +
+                '}';
+    }
+}
