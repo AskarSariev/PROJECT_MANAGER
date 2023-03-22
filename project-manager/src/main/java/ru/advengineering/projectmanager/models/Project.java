e433195a5115a04e6ff1f8a973a482fc098a2dc5
Askar Sariev <askar-64@mail.ru>
2023-03-22 21:38:54 +0300

    Add Entity class Project

---
+++ project-manager/src/main/java/ru/advengineering/projectmanager/models/Project.java
@@ -0,0 +1,59 @@
+package ru.advengineering.projectmanager.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "parent_project_id")
+    private int parentProjectId;
+
+    public Project() {
+    }
+
+    public Project(String name, int parentProjectId) {
+        this.name = name;
+        this.parentProjectId = parentProjectId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getParentProjectId() {
+        return parentProjectId;
+    }
+
+    public void setParentProjectId(int parentProjectId) {
+        this.parentProjectId = parentProjectId;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", parentProjectId=" + parentProjectId +
+                '}';
+    }
+}
