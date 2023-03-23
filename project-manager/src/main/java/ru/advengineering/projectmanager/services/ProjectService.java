96f775f1cab5e801b99d5b23a904bbbef4279f99
Askar Sariev <askar-64@mail.ru>
2023-03-23 06:21:51 +0300

    Add ProjectService and TaskService

---
+++ project-manager/src/main/java/ru/advengineering/projectmanager/services/ProjectService.java
@@ -0,0 +1,29 @@
+package ru.advengineering.projectmanager.services;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.advengineering.projectmanager.models.Project;
+import ru.advengineering.projectmanager.repositories.ProjectRepository;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    @Transactional
+    public void saveProject(Project project) {
+        projectRepository.save(project);
+    }
+}
---
+++ project-manager/src/main/java/ru/advengineering/projectmanager/services/TaskService.java
@@ -0,0 +1,29 @@
+package ru.advengineering.projectmanager.services;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.advengineering.projectmanager.models.Task;
+import ru.advengineering.projectmanager.repositories.TaskRepository;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> findAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @Transactional
+    public void saveTask(Task task) {
+        taskRepository.save(task);
+    }
+}
