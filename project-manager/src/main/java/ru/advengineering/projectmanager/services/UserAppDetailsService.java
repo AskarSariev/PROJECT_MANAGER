47cf9532b1dfcd169ca6e4ac2f15cd924ca1e3ac
Askar Sariev <askar-64@mail.ru>
2023-03-28 20:30:59 +0300

    Create class UserAppDetailsService

---
+++ project-manager/src/main/java/ru/advengineering/projectmanager/services/UserAppDetailsService.java
@@ -0,0 +1,34 @@
+package ru.advengineering.projectmanager.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.advengineering.projectmanager.models.UserApp;
+import ru.advengineering.projectmanager.repositories.UserAppRepository;
+import ru.advengineering.projectmanager.security.UserAppDetails;
+
+import java.util.Optional;
+
+@Service
+public class UserAppDetailsService implements UserDetailsService {
+
+    private final UserAppRepository userAppRepository;
+
+    @Autowired
+    public UserAppDetailsService(UserAppRepository userAppRepository) {
+        this.userAppRepository = userAppRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserApp> userApp = userAppRepository.findByUsername(username);
+
+        if (userApp.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new UserAppDetails(userApp.get());
+    }
+}
